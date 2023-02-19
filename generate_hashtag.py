2d740a6ff8faae0c136a8e942cb691eefbccee5c
Nicholas Parker <nicholas.st.parker@gmail.com>
2023-02-18 20:00:22 -0600

    more challenges

---
+++ generate_hashtag.py
@@ -0,0 +1,9 @@
+def generate_hashtag(s):
+    if not s or len(s) > 140:
+        return False
+    s = s.lower().split()
+    for count, word in enumerate(s):
+        s[count] = word.capitalize()
+    return f"#{''.join(s)}"
+
+print(generate_hashtag("    Codewars      is nice "))
---
+++ triboncci.py
@@ -0,0 +1,14 @@
+def tribonacci(signature, n):
+    if n == 0:
+        return []
+    if n == 1:
+        return [signature[0]]
+    if n == 2:
+        return [signature[0], signature[1]]
+    list_copy = signature.copy()
+    for i in range(n - 3):
+        list_copy.append(sum(list_copy[-3:]))
+    return list_copy
+
+
+print(tribonacci([1, 1, 1], 30))

5bc1779759fdd4f862b2569f0cb7a58bc174c0e2
Nicholas Parker <nicholas.st.parker@gmail.com>
2023-02-21 21:48:04 -0600

    added more challenges

---
+++ move_zeroes.py
@@ -0,0 +1,13 @@
+# take a list and move its zeroes to the end while keeping order
+
+
+def move_zeros(lst):
+    zeros = []
+    for i in range(len(lst)-1, -1, -1):
+        if lst[i] == 0:
+            zeros.append(lst.pop(i))
+    lst.extend(zeros)
+    return lst
+
+
+print(move_zeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]))
